1d5686095eba0ba7d0679b7c4d7b0c64e6f9d0c8
Bogdan <yolgin.bogdan@yandex.ru>
2024-03-20 20:52:14 +0700

    add src

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<nav>
+    <div class="nav-container">
+    <a href="/" class="nav-logo" title="Back to Homepage">Елгин Богдан</a>
+    <div class="nav-links">
+    <a href="/about" class="link">Мне 17 лет, проживаю в городе Юрга.</a>
+    <a href="/blog" class="link">что-то</a>
+    <a href="/projects" class="link">Пока не придумал</a>
+    <a href="/contacts" class="link">Номер телефона: +79001023536</a>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+<!--Pags will be injected below-->
+<slot></slot>
+</div>
+
+<style>
+    .container {
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+</style>
